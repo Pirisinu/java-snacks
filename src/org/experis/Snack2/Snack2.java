4ef5554e08ff74221aaaf889f702b497fbe6c100
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-28 14:02:49 +0100

    init Snack2

---
+++ src/org/experis/Snack2/Snack2.java
@@ -0,0 +1,17 @@
+package org.experis.Snack2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Snack2 {
+    public static void main(String[] args) {
+        String[] nomi = {"Mario", "Luigi", "Giovanni", "Marco", "Paolo", "Giuseppe", "Francesco", "Luca", "Andrea", "Alessandro", "Antonio", "Roberto", "Davide", "Simone", "Stefano"};
+        String[] cognomi = {"Rossi", "Bianchi", "Ferrari", "Esposito", "Russo", "Romano", "Gallo", "Costa", "Fontana", "Conti", "Barbieri", "Greco", "Marino", "Galli", "Moretti"};
+
+        String[] fakeList = new String[nomi.length];
+
+        Random random = new Random();
+
+
+    }
+}
