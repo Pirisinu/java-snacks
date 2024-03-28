ec86e3b029d5f498264e10cf970f2a2c39d950fe
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-28 14:53:14 +0100

    get input from user

--- src/org/experis/Snack4/Snack4.java
+++ src/org/experis/Snack4/Snack4.java
@@ -1,7 +1,15 @@
 package org.experis.Snack4;
 
+import java.util.Scanner;
+
 public class Snack4 {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Seleziona la parola da dover per vedere se è palindroma: ");
+        String selectedWord = scan.nextLine();
+
+        System.out.println(selectedWord);
     }
 }
