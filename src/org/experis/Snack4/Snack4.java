c378e7123ef594d84b0e8e740092effb8921b266
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-28 15:08:25 +0100

    add palindrome logic

--- src/org/experis/Snack4/Snack4.java
+++ src/org/experis/Snack4/Snack4.java
@@ -10,6 +10,23 @@ public class Snack4 {
         System.out.print("Seleziona la parola da dover per vedere se è palindroma: ");
         String selectedWord = scan.nextLine();
 
-        System.out.println(selectedWord);
+        boolean isPalindromo = false;
+        char[] myChar = selectedWord.toCharArray();
+
+        // controllo che la prima lettera sia diversa dall'ultima e incremento per la prima lettera e decremento per l'ultima
+        int j = selectedWord.length() - 1;
+        for (int i = 0; i < selectedWord.length(); i++) {
+            if (myChar[i] != myChar[j]) {
+                isPalindromo = false;
+                break;
+            } else {
+                j--;
+                isPalindromo = true;
+            }
+        }
+
+        String message = (isPalindromo) ? "La stringa è palindroma." : "La stringa non è palindorma.";
+        System.out.println(isPalindromo);
+
     }
 }
