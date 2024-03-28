9d6fcdb843880e6bc6c4c01cd4bc31bbb1c2f648
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-03-28 14:12:37 +0100

    add all names with all surname

--- src/org/experis/Snack2/Snack2.java
+++ src/org/experis/Snack2/Snack2.java
@@ -7,10 +7,20 @@ public class Snack2 {
     public static void main(String[] args) {
         String[] nomi = {"Mario", "Luigi", "Giovanni", "Marco", "Paolo", "Giuseppe", "Francesco", "Luca", "Andrea", "Alessandro", "Antonio", "Roberto", "Davide", "Simone", "Stefano"};
         String[] cognomi = {"Rossi", "Bianchi", "Ferrari", "Esposito", "Russo", "Romano", "Gallo", "Costa", "Fontana", "Conti", "Barbieri", "Greco", "Marino", "Galli", "Moretti"};
+        int totalFakeName = nomi.length * cognomi.length;
+        String[] fakeList = new String[totalFakeName];
 
-        String[] fakeList = new String[nomi.length];
+        int fakeListIndex = 0;
+        for (int i = 0; i < nomi.length; i++) {
+            for (int j = 0; j < cognomi.length; j++) {
+                fakeList[fakeListIndex] = nomi[i] + " " + cognomi[j];
+                fakeListIndex++;
+            }
+        }
+        System.out.println(Arrays.toString(fakeList));
 
-        Random random = new Random();
+
+
 
 
     }
